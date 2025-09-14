076d64b84515a7ef65bfdf0e83f6ef6999063e38
Kanheiya Goswami <kggoswami0709@gmail.com>
2025-09-14 22:59:45 +0530

    Add files via upload

---
+++ MoveAllZeroesToEnd.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MoveAllZeroesToEnd {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        int n = arr.length;
+        int count = 0; 
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i]; 
+            }
+        }
+        while (count < n) {
+            arr[count++] = 0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
