52c95d0da195c4563a1f55f1afc5a3d60e02d77d
Kanheiya Goswami <kggoswami0709@gmail.com>
2025-10-13 10:21:40 +0530

    Add files via upload

---
+++ productofarrayexceptself.java
@@ -0,0 +1,27 @@
+public class productofarrayexceptself {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5};
+        int n = arr.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        int prod[] = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * arr[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * arr[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            prod[i] = left[i] * right[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(prod[i] + " ");
+        }
+    }
+}
